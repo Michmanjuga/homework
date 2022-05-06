060bcb5b3945417b4207aad83a5f5ee5fe133621
Michael Chistyukhin <living.in.russia@gmail.com>
2022-05-06 09:36:41 +0200

    Addendum

---
+++ container.hpp
@@ -0,0 +1,21 @@
+
+#include <list>
+
+namespace my
+{
+template<typename T>
+struct node {
+    T value;
+    node* next;
+};
+
+class fwd_list
+{
+    
+public:
+    fwd_list() = default;
+
+};
+
+
+}
