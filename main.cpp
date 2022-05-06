cc7eca94ef327b570adc707182e4aeec333b5331
Michael Chistyukhin <living.in.russia@gmail.com>
2022-05-06 09:34:53 +0200

    Minor changes

--- main.cpp
+++ main.cpp
@@ -20,23 +20,34 @@
 
 template<size_t number>
 struct factorial {
-    static const size_t value = number * factorial<number - 1>::value;
+    static constexpr size_t value = number * factorial<number - 1>::value;
 };
 
 template<>
 struct factorial<0ul> {
-    static const size_t value = 1ul;
+    static constexpr size_t value = 1ul;
 };
 
 
 int main(int, char**)
 {
-    std::map<int, int, std::less<int>, custom::allocator<std::pair<const int, int>>> fact;
-    for (size_t i = 1; i < 11; ++i) {
-        static size_t factorial = 1ul;
-        factorial *= i;
-        fact.emplace( i, factorial );
-    }
+    std::map<
+        int,
+        int,
+        std::less<int>,
+        custom::allocator<std::pair<const int, int>>
+    > fact;
+    
+    fact.emplace( 0, factorial<0>::value );
+    fact.emplace( 1, factorial<1>::value );
+    fact.emplace( 2, factorial<2>::value );
+    fact.emplace( 3, factorial<3>::value );
+    fact.emplace( 4, factorial<4>::value );
+    fact.emplace( 5, factorial<5>::value );
+    fact.emplace( 6, factorial<6>::value );
+    fact.emplace( 7, factorial<7>::value );
+    fact.emplace( 8, factorial<8>::value );
+    fact.emplace( 9, factorial<9>::value );
     
     
     std::cout << fact << std::endl;
